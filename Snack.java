63be076e1c45a527bce33006a646fb4fc49cc5dc
jstn131 <jh013107@icloud.com>
2026-02-20 16:54:10 -0800

    Created base for snack

--- Snack.java
+++ Snack.java
@@ -1,3 +1,21 @@
 public class Snack {
-    
+    private String name;
+    private String flavor;
+
+    public Snack(String name, String flavor)
+    {
+        this.name = name;
+        this.flavor = flavor;
+    }
+
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getFalvor()
+    {
+        return flavor;
+    }
 }
