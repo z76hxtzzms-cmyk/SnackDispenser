58627329750c6f95c36c92a42e529c00a3b06bcd
jstn131 <jh013107@icloud.com>
2026-02-20 16:57:02 -0800

    Added Javadoc

--- SnackDispenser.java
+++ SnackDispenser.java
@@ -2,17 +2,32 @@ public class SnackDispenser {
 
     private BagInterface<Snack> snacks;
 
+    /*
+     * Constructor:
+     * Creates an empty snack dispenser.
+     */
     public SnackDispenser() {
         snacks = new ArrayBag<>();
     }
 
+    /**
+     * Adds a snack to the dispenser.
+     * 
+     * @param s The snack to be added.
+     * @return True if the snack was added successfully, or false if not.
+     */
     public boolean addSnack(Snack s) {
-        return snacks.add(s);
+        return snacks.add(s); // Uses ArrayBag's add method to add the snack to the bag
     }
 
-    public Snack removeSnack() {
-        return snacks.remove();
+    /**
+     * Removes a snack from the dispenser.
+     * 
+     * @return The removed snack if successful, or null if not.
+     */
 
+    public Snack removeSnack() {
+        return snacks.remove(); // Uses ArrayBag's remove method to remove an unspecified snack from the bag
     }
 
     public int countFlavor(String flavor) {
@@ -20,8 +35,7 @@ public class SnackDispenser {
         return 0;
     }
 
-    public void printAllSnacks()
-    {
+    public void printAllSnacks() {
         // Will implement this later
     }
 
